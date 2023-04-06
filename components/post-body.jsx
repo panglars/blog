69874028316e53af0e64f9eec51c370c235612c7
ShaMaTeTuanZhang <i@yinn.party>
2023-04-06 22:43:07 +0800

    add post css

--- components/post-body.jsx
+++ components/post-body.jsx
@@ -1,6 +1,8 @@
+import orgStyles from "./org-styles.module.scss";
+
 export default function Postbody({ content }) {
   return (
-    <div>
+    <div className={orgStyles["org"]}>
       <div dangerouslySetInnerHTML={{ __html: content }} />
     </div>
   );
