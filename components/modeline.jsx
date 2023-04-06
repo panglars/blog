7b4499e6907569353d227f11b92eae48606bca5a
ShaMaTeTuanZhang <i@yinn.party>
2023-04-06 22:43:28 +0800

    add Modeline css

--- components/modeline.jsx
+++ components/modeline.jsx
@@ -1,12 +1,13 @@
+import styles from "./modeline.module.scss";
 import Link from "next/link";
 
 export default function Modeline({ bufferName = "Buffer" }) {
   return (
-    <>
-      <div className="Modeline">PangLAN {bufferName} "progress"</div>
+    <div className={styles.modeline}>
+      <div>PangLAN {bufferName} "progress"</div>
       <Link href="/"> Home </Link>
       <Link href="/about"> About </Link>
       <Link href="/firends"> Firends </Link>
-    </>
+    </div>
   );
 }
