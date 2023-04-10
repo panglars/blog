69bcd58e98072fbd4fdd2fffb953ba636d4ca782
ShaMaTeTuanZhang <i@yinn.party>
2023-04-10 22:40:09 +0800

    Add code highlight

--- components/post-body.jsx
+++ components/post-body.jsx
@@ -1,9 +1,28 @@
 import orgStyles from "./org-styles.module.scss";
+import rehypeHighlight from "rehype-highlight";
+import { rehype } from "rehype";
+import { useEffect, useState } from "react";
+import "highlight.js/styles/base16/solarized-light.css";
 
 export default function Postbody({ content }) {
+  const file = rehype()
+    .data("settings", { fragment: true })
+    .use(rehypeHighlight)
+    .process(content);
+
+  const [highlightContext, setHighlightContext] = useState(content);
+
+  useEffect(() => {
+    file.then((vfileObject) => {
+      const value = vfileObject.value;
+      console.log(String(value));
+      setHighlightContext(value);
+    });
+  }, []);
+
   return (
     <div className={orgStyles["org"]}>
-      <div dangerouslySetInnerHTML={{ __html: content }} />
+      <div dangerouslySetInnerHTML={{ __html: highlightContext }} />
     </div>
   );
 }
