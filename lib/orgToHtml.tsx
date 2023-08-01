f38d2beb64843a65d628a44c2d6dd7e25a172d50
PangLAN <i@yinn.party>
2023-08-01 12:40:54 +0800

    Feat: Add uniorg

---
+++ lib/orgToHtml.tsx
@@ -0,0 +1,17 @@
+import { unified } from "unified";
+import html from "rehype-stringify";
+
+import uniorg from "uniorg-parse";
+import uniorg2rehype from "uniorg-rehype";
+import extractKeywords from "uniorg-extract-keywords";
+
+const processor = unified()
+  .use(uniorg)
+  .use(extractKeywords)
+  .use(uniorg2rehype)
+  .use(html);
+
+export default function orgToHtml(org: any) {
+  const result = processor.processSync(org);
+  return result;
+}
---
+++ lib/posts.tsx
@@ -0,0 +1,53 @@
+import fs from "fs";
+import { join } from "path";
+import orgToHtml from "./orgToHtml";
+
+const postsDirectory = join(process.cwd(), "posts");
+
+export function getPostSlugs(): string[] {
+  return fs.readdirSync(postsDirectory);
+}
+
+export function getPostBySlug(
+  slug: string,
+  fields: string[] = []
+): Record<string, any> {
+  const realSlug = slug.replace(/\.org$/, "");
+  const fullPath = join(postsDirectory, `${realSlug}.org`);
+  const content = fs.readFileSync(fullPath, "utf8");
+  const org = orgToHtml(content);
+
+  const items: Record<string, any> = {};
+
+  // Ensure only the minimal needed data is exposed
+  fields.forEach((field) => {
+    if (field === "slug") {
+      items[field] = realSlug;
+    }
+    if (field === "content") {
+      items[field] = String(org);
+    }
+    if (org.data[field]) {
+      items[field] = org.data[field];
+    }
+  });
+
+  return items;
+}
+
+// export function getAllPosts(fields: string[] = []): Record<string, any>[] {
+//   const slugs = getPostSlugs();
+//   const posts = slugs
+//     .map((slug) => getPostBySlug(slug, fields))
+//     // sort posts by date in descending order
+//     .sort((post1, post2) => (post1.date > post2.date ? "-1" : "1"));
+//   return posts;
+// }
+//
+export function getAllPosts(fields: string[] = []): Record<string, any>[] {
+  const slugs = getPostSlugs();
+  const posts = slugs
+    .map((slug) => getPostBySlug(slug, fields))
+    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
+  return posts;
+}
