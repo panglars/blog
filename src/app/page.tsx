12d6d4fa97c35838399c51d2c146de43805e2920
PangLAN <i@yinn.party>
2023-09-19 15:47:20 +0800

    Add app router

---
+++ src/app/page.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import { getAllPosts } from "../lib/posts";
+
+export default async function Page() {
+  const allPosts = await getPostInfo();
+  //console.log(allPosts);
+  return (
+    <>
+      <h1 className="text-center text-2xl font-sans m-2">
+        Hello,I'm Pang LAN{" "}
+      </h1>
+      <h2 className="text-center text-xl">都不知道该干什么</h2>
+      <ul className="">
+        {allPosts.map((post) => (
+          <li className="list-item" key={post.date}>
+            <div>
+              <Link href={`/post/${post.slug}`}>{post.title}</Link>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <Link href="/about" className="text-left">
+        About{" "}
+      </Link>
+    </>
+  );
+}
+
+async function getPostInfo() {
+  const allPosts = getAllPosts([
+    "title",
+    "date",
+    "slug",
+    "author",
+    "category",
+    "tags",
+    "content",
+  ]);
+  return allPosts;
+}
