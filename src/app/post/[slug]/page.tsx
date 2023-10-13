935597ce89c8eeb835e479d97d42b42ac17621f0
PangLAN <i@yinn.party>
2023-10-13 12:22:31 +0800

    Support dynamic Metadata

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,24 +1,38 @@
 import PostBody from "../../../components/postbody";
-import {getAllPosts, getPostBySlug} from "@/lib/posts";
+import { getAllPosts, getPostBySlug } from "@/lib/posts";
+import type { Metadata } from "next";
+
+type Props = {
+  params: { id: string };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  // read route params
+  const post = await getPost(params);
+
+  return {
+    title: post.title,
+  };
+}
 
 export async function generateStaticParams() {
-    return getAllPosts(["slug"]);
+  return getAllPosts(["slug"]);
 }
+
 async function getPost(params: any) {
-    return getPostBySlug(params.slug, [
-      "title",
-      "date",
-      "slug",
-      "language",
-      "category",
-      "tags",
-      "content",
+  return getPostBySlug(params.slug, [
+    "title",
+    "date",
+    "slug",
+    "language",
+    "category",
+    "tags",
+    "content",
   ]);
 }
 
-export default async function Post({ params }: any) {
+export default async function Post({ params }: Props) {
   const post = await getPost(params);
-  // console.log(post);
   return (
     <>
       <h1 className="text-center text-3xl">{post.title}</h1>
