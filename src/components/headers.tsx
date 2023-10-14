75080d3afda80a3e9838a15a383b6366b67a54c1
PangLAN <i@yinn.party>
2023-10-14 12:27:23 +0800

    Add fontawesome package and change Headers icon

--- src/components/headers.tsx
+++ src/components/headers.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { ModeToggle } from "@/components/mode-toggle";
-
 export default function Headers() {
   return (
     <header>
@@ -13,7 +12,6 @@ export default function Headers() {
         <nav className="ml-auto font-medium space-x-6">
           <Link href="/">Home</Link>
           <Link href="/about">About</Link>
-          <Link href="/rss.xml">RSS</Link>
         </nav>
       </div>
     </header>
