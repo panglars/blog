6978b7023e1c7a67ecf74f51fb6a48144492f835
PangLAN <i@yinn.party>
2023-10-19 23:09:13 +0800

    Add Friends components

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,8 @@
 "use client";
-
 import useComment from "@/components/useComment";
 import CommentBox from "@/components/CommentBox";
 import CommentList from "@/components/CommentList";
+import Firends from "@/components/friends";
 
 export default function Page() {
   //const pathname = usePathname();
@@ -13,11 +13,10 @@ export default function Page() {
         About Me
       </h1>
       <p> Building...</p>
-
       <h2 className="text-3xl leading-snug my-12 text-slate-900 dark:text-slate-100">
         My Firend Links
       </h2>
-
+      <Firends />
       <div>
         <CommentBox onSubmit={onSubmit} text={text} setText={setText} />
         <CommentList comments={comments} onDelete={onDelete} />
