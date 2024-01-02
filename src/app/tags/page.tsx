3b9db84ecec314cabbd7264a44bcd15f902fe637
PangLAN <i@yinn.party>
2024-01-02 13:27:37 +0800

    feat: update tags page

--- src/app/tags/page.tsx
+++ src/app/tags/page.tsx
@@ -1,8 +1,10 @@
 import PostList from "@/layouts/PostListWithTags";
-import { getAllPosts } from "@/lib/posts";
+import { getAllTags } from "@/lib/posts";
+import Tag from "@/components/Tag";
+import Link from "@/components/Link";
 
 export default function Tags() {
-  const allPosts = getAllPosts([
+  const allTags = getAllTags([
     "title",
     "date",
     "slug",
@@ -11,6 +13,31 @@ export default function Tags() {
     "tags",
     "content",
   ]);
-
-  return <PostList displayPosts={allPosts} tagTitle={"AllPosts"} />;
+  return (
+    <>
+      <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
+        <div className="space-x-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14">
+            Tags
+          </h1>
+        </div>
+        <div className="flex max-w-lg flex-wrap">
+          {allTags.map((tag) => {
+            return (
+              <div key={tag} className="mb-2 mr-5 mt-2">
+                <Tag text={tag} />
+                <Link
+                  href={`/tags/${slug(tag)}`}
+                  className="-ml-2 text-sm font-semibold uppercase text-gray-600 dark:text-gray-300"
+                  aria-label={`View posts tagged ${tag}`}
+                >
+                  {` (${tagCounts[tag]})`}
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
